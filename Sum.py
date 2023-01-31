8f5eba827e4caf1cdba79c05247c75cfbc8152c4
Alen Mathew <alen.mathew@kovenantz.com>
2023-01-31 21:13:33 +0000

    format documents

--- Sum.py
+++ Sum.py
@@ -1,5 +1,7 @@
-def Sum(a:list,l:list):
-    listNumbers = list(filter(lambda x: len([y for y in a if x%y==0])>0,l))
+def Sum(a: list, l: list):
+    listNumbers = list(filter(lambda x: len(
+        [y for y in a if x % y == 0]) > 0, l))
     return sum(listNumbers)
 
-print(Sum(list([3,5,7]),list([1,2,3,4,5,6,7,8,9,10])))
+
+print(Sum(list([3, 5, 7]), list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])))
