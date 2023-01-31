e2e247cae26593ca7c4ca9c2d135b8d91a7d58d0
Alen Mathew <alen.mathew@kovenantz.com>
2023-01-31 21:07:23 +0000

    PY ex3.3

--- Sum.py
+++ Sum.py
@@ -1,6 +1,5 @@
-def Sum(a,b,n):
-    listNumbers = list(range(1,n+1))
-    listNumbers = list(filter(lambda x: (((x %a==0) or (x % b ==0)) and x<n),listNumbers))
+def Sum(a,b,l):
+    listNumbers = list(filter(lambda x: (((x %a==0) or (x % b ==0)) ),l))
     return sum(listNumbers)
 
-print(Sum(3,5,10))
+print(Sum(3,5,list([1,2,3,4,5,6,7,8,9,10])))
