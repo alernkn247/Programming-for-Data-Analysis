f9d0c91bdff6ca6ed9e7b64191589e5964ced4d9
Alen Mathew <alen.mathew@kovenantz.com>
2023-01-31 21:06:08 +0000

    PY ex3.2

--- Sum.py
+++ Sum.py
@@ -1,5 +1,6 @@
-def Sum(x):
-    result = x*(x+1)/2
-    return int(result)
+def Sum(a,b,n):
+    listNumbers = list(range(1,n+1))
+    listNumbers = list(filter(lambda x: (((x %a==0) or (x % b ==0)) and x<n),listNumbers))
+    return sum(listNumbers)
 
-print(Sum(3))
+print(Sum(3,5,10))
